f497f024298662a3134f75c22c235302312394ff
jamesishimwe <james123ishimwe@gmail.com>
2026-03-10 10:58:26 +0100

    rename variables and start with a letter

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+// The above variable names are invalid because variable names cannot start with a number.
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
