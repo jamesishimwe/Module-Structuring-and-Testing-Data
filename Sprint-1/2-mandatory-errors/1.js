24827db8f30d531fa53c5ff056091730267e0790
jamesishimwe <james123ishimwe@gmail.com>
2026-03-10 10:44:59 +0100

    declare age using let instead of const

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,4 @@
 // trying to create an age variable and then reassign the value by 1
-
-const age = 33;
+// age is declared as a variable using let, which allows us to reassign its value later on. Initially, age is set to 33. Then, we reassign age by adding 1 to its current value, resulting in age being updated to 34.
+let age = 33;
 age = age + 1;
