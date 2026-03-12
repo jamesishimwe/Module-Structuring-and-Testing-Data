554f516f28fc8920df98a997609677ec304025f0
jamesishimwe <james123ishimwe@gmail.com>
2026-03-12 20:15:51 +0100

    declare and assign initials variable at the same time

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,9 +5,10 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
-initials = firstName.substring(0, 1) + middleName.substring(0, 1) + lastName.substring(0, 1);
-console.log(initials);  
+let initials =
+  firstName.substring(0, 1) +
+  middleName.substring(0, 1) +
+  lastName.substring(0, 1);
+console.log(initials);
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
-
