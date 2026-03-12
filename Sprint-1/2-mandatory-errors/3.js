d8527c9c375eec871268e99747f5589eb0e998e0
jamesishimwe <james123ishimwe@gmail.com>
2026-03-12 20:21:13 +0100

    change cardNumber variable to string

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,11 +1,11 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
 //const last4Digits = cardNumber.toString().slice(-4);
 //console.log(last4Digits);
 
 // Prediction: The code will not work because cardNumber is a number and the slice method is not applicable to numbers. It will likely throw a TypeError.
 // After running the code, it gives a TypeError: cardNumber.toString(...).slice is not a function. This is because toString() returns a string, and slice is a method for strings, but we are trying to call slice on the result of toString() which is not correct.
 // To fix this, we need to ensure that we are calling slice on the string representation of cardNumber. We can do this by first converting cardNumber to a string and then slicing it. The updated code would be:
-const last4Digits = cardNumber.toString().slice(-4);
+const last4Digits = cardNumber.slice(-4);
 //console.log(last4Digits); // This should now correctly output the last 4 digits of the card number.
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
