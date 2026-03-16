3d55010938829730468171f9a15979ae238f4dac
jamesishimwe <james123ishimwe@gmail.com>
2026-03-16 16:31:22 +0100

    Comment out the old code

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,13 +1,13 @@
 // Predict and explain first...
 
 // The function multiply is supposed to return the product of a and b, but it currently does not return anything. Instead, it only logs the product to the console. Therefore, when we call multiply(10, 32) inside the template literal, it will log 320 to the console but will return undefined.Then when we try to use the result of multiply(10, 32) in the template literal, it will output "The result of multiplying 10 and 32 is undefined" because the function does not return a value.
-
+/*
 function multiply(a, b) {
   console.log(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
+*/
 // To fix the problem, we need to make sure that the multiply function returns the product of a and b instead of just logging it to the console. We can do this by replacing console.log with a return statement.
 
 // Finally, correct the code to fix the problem
