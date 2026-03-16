92ecb9a1a266b029bd10c75a674d1faafc5ed33e
jamesishimwe <james123ishimwe@gmail.com>
2026-03-16 16:34:13 +0100

    Predict what happens and why the error occurs

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
-
+// The function sum is supposed to return the sum of a and b, by getting a and b as parameters, add the two then return the sum but it currently does not return anything. Instead, it has a return statement with no value, which means it will return undefined. Therefore, when we call sum(10, 32) inside the template literal, it will return undefined and the output will be "The sum of 10 and 32 is undefined".
 function sum(a, b) {
   return;
   a + b;
