5ca91435ddc06c911d291a390a0573b229edf7e4
jamesishimwe <james123ishimwe@gmail.com>
2026-03-16 16:00:33 +0100

    Predict what happens after execution

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  The function capitalise takes a string argument and is supposed to return the string with the first letter capitalised by getting the first letter and capitalising it then adding the rest of the string. However, there is an error in the code because the variable name 'str' is being redeclared twice, first as a parameter and then inside the function, which causes a conflict. The error message will likely indicate that 'str' has already been declared. To fix this, we can simply remove the redeclaration and directly modify the input string.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
