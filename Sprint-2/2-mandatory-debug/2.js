8e5a3b52188d2ab8f90e7bc0cd14f7166e54d761
jamesishimwe <james123ishimwe@gmail.com>
2026-03-16 16:41:32 +0100

    Predict what happens

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// This code will not work as expected because the function getLastDigit does not take any parameters, but we are trying to pass a number to it when we call it. The function is currently using a constant variable num which is set to 103, so it will always return the last digit of 103, which is 3.
 
 const num = 103;
 
@@ -14,7 +14,7 @@ console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============> write the output her
 // Explain why the output is the way it is
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
