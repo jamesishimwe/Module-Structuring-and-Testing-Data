8a58053f63310c719dca61f2f43aad5b02301b9f
jamesishimwe <james123ishimwe@gmail.com>
2026-03-16 16:18:41 +0100

    Comment out the old code

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,25 +1,22 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============>  This code will give an error because the parameter name '3' is not a valid variable name in JavaScript. Variable names cannot start with a number, and '3' is not a valid identifier. This will result in a SyntaxError when the code is executed. To fix this, we need to use a valid variable name for the parameter that starts with a letter or an underscore.
-
+/*
 function square(3) {
     return num * num;
 }
-
+*/
 // SyntaxError: Unexpected number
 
 // =============> explain this error message here
 
 // Finally, correct the code to fix the problem
 
-// Here's the corrected code:   
+// Here's the corrected code:
 function square(num) {
-    return num * num;
+  return num * num;
 }
 
 // Now the function should work correctly and return the square of any number passed as an argument.
-
-
