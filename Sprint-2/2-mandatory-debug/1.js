59db7cf94a3fb9151cde5fe050db875a976e7dbb
jamesishimwe <james123ishimwe@gmail.com>
2026-03-16 16:36:53 +0100

    Write new code that works

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -11,3 +11,8 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
