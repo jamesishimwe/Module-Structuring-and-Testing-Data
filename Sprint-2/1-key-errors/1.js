acd8e5dd6af284389562ac1fd1715716e243352d
jamesishimwe <james123ishimwe@gmail.com>
2026-03-16 16:08:25 +0100

    Predict what happens and why the error occurs

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// The function convertToPercentage takes a decimal number as an argument and is supposed to return the percentage equivalent of that number. However, the variable 'decimalNumber' is being declared twice, once as a parameter and once inside the function. It is also being assigned a value inside the function, which is different from the parameter value. This causes a conflict and results in an error message indicating that 'decimalNumber' has already been declared, and it would also give a different percentage value which is not equal to the initial value. To fix this, we can remove the redeclaration and directly modify the input variable without declaring a new one.
 
 // Try playing computer with the example to work out what is going on
 
