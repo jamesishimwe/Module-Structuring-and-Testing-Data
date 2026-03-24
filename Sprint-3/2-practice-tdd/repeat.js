c0e3b2ed8880818be520185bff62c08349aca187
jamesishimwe <james123ishimwe@gmail.com>
2026-03-24 19:22:53 +0100

    Implement function repeat

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,9 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) return "Invalid times";
+  return Array(count)
+    .fill(0)
+    .map(() => str)
+    .join("");
 }
 
 module.exports = repeat;
