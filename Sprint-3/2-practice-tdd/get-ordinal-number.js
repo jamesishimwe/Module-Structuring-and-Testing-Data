00bcbf2a7179245f6d5efb4261b3b9fb4a09369b
jamesishimwe <james123ishimwe@gmail.com>
2026-03-24 19:05:14 +0100

    Implement the function getOrdinalNumber

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,12 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (num === 11) return "11th";
+  else if (num === 12) return "12th";
+  else if (num === 13) return "13th";
+  numLast = num.toString().slice(-1);
+  if (numLast === "1") return `${num}st`;
+  else if (numLast === "2") return `${num}nd`;
+  else if (numLast === "3") return `${num}rd`;
+  else return `${num}th`;
 }
 
 module.exports = getOrdinalNumber;
