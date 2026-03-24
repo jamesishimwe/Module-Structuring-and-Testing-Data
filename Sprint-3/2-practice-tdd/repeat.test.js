f23f89e0f7a80b868501f8e24d8d4ea7dfc1ea3e
jamesishimwe <james123ishimwe@gmail.com>
2026-03-24 19:24:56 +0100

    Add test for count 1

--- Sprint-3/2-practice-tdd/repeat.test.js
+++ Sprint-3/2-practice-tdd/repeat.test.js
@@ -20,7 +20,12 @@ test("should repeat the string count times", () => {
 // Given a target string str and a count equal to 1,
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
-
+test("should repeat the string count times", () => {
+  const str = "hello";
+  const count = 1;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("hello");
+});
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
