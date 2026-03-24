7b893416087eb651ec45055a37a5e47e92762fd1
jamesishimwe <james123ishimwe@gmail.com>
2026-03-24 18:02:07 +0100

    add jest test for improper fraction

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,9 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
-
+test("should return false for a proper fraction", () => {
+  expect(isProperFraction(3, 2)).toEqual(false);
+});
 // Case 3: Identify Negative Fractions:
 
 // Case 4: Identify Equal Numerator and Denominator:
